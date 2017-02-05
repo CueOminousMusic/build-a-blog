109f4693cdd60115113fa7b4b536c3106728e0d8
CueOminousMusic <mherzber@gmail.com>
2017-02-04 20:28:54 -0600

    Added handlers for /blog and /newpost.  Added Jinja2

--- main.py
+++ main.py
@@ -15,11 +15,34 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
+
+class BlogDisplay(webapp2.RequestHandler):
+    def get(self):
+        t = jinja_env.get_template("blogdisplay.html")
+        content = t.render()
+        self.response.write(content)
+
+class NewPost(webapp2.RequestHandler):
+    def post(self):
+        #self.request.get('Stuff')
+        t = jinja_env.get_template("newpost.html")
+        content = t.render()
+        self.response.write(content)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/blog', BlogDisplay),
+    ('/newpost', NewPost)
 ], debug=True)
